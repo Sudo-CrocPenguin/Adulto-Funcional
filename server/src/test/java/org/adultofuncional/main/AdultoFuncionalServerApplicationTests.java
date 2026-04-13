7b88fb7c2eaebac4c0b4aae84905a85b4691520a
Jeanzo1719 <sebasrios1119@gmail.com>
2026-04-13 18:07:08 -0500

    chore: generación del proyecto spring boot

---
+++ server/src/test/java/org/adultofuncional/main/AdultoFuncionalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adultofuncional.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdultoFuncionalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
