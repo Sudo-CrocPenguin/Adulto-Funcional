cb50f14564ef757a705931a1675abe61e785b79f
Sudo-CrocPenguin <blandonmontesmiguelangel@gmail.com>
2026-05-07 02:38:35 -0500

    feat: crear esqueleto básico para pantallas de autenticación

---
+++ movil/app/(auth)/login.tsx
@@ -0,0 +1,16 @@
+/**
+ * Pantalla de inicio de sesión.
+ * Permite al usuario autenticarse con email y contraseña.
+ *
+ * @author Miguel Angel Blandon Montes
+ */
+
+import { Text, View } from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Pantalla de Login - Próximamente</Text>
+    </View>
+  );
+}
